61ef62b57c54390dd6626a46e27e26d12b7f209a
Rishit Khandelwal <65018379+rishit-khandelwal@users.noreply.github.com>
2020-09-02 11:54:27 +0530

    :'(

--- gb.js
+++ gb.js
@@ -5,7 +5,9 @@ client.on('message', async (message) => {
 	if (!message.author.bot) { // The message is not sent by a bot
 
 		let content = message.content;	// The conents of the message
-		if (content == "!ping")
+		if (content == "!ping") {
+			message.channel.send("!pong") // Send `!pong` in the channel where `!ping was sent`
+		}
 	}
 })
 
